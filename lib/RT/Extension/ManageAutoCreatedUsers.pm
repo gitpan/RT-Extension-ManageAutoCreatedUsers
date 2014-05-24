ec4a7b06d472ca3bbaa1825228dc9b3925349164
Wallace Reis <wallace@reis.me>
2014-05-24 15:58:00 +0000

    Import of WREIS/RT-Extension-ManageAutoCreatedUsers-0.05 from CPAN.
    gitpan-cpan-distribution: RT-Extension-ManageAutoCreatedUsers
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         WREIS/RT-Extension-ManageAutoCreatedUsers-0.05.tar.gz
    gitpan-cpan-author:       WREIS
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/ManageAutoCreatedUsers.pm
+++ lib/RT/Extension/ManageAutoCreatedUsers.pm
@@ -6,7 +6,7 @@ use RT::Extension::MergeUsers;
 use Scalar::Util 'blessed';
 use Module::Runtime 'use_module';
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 RT->AddJavaScript('manage-autocreated-users.js');
 
